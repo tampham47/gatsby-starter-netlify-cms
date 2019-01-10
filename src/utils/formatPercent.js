5f4dc032d0fc2d47c6561509c9cfbd3289e47adc
Tam Pham <tampham47@live.com>
2019-01-10 18:20:00 +0700

    enhance percentage format by hidding value from unstable market (#8)

--- src/utils/formatPercent.js
+++ src/utils/formatPercent.js
@@ -1,1 +1,7 @@
-export default value => `${Math.round(value * 10000) / 100}`;
+// the 24h percentage change over 100 mean it's not stable
+// dont recommended to trade on those
+export default value => {
+  const percent = Math.round(value * 10000) / 100;
+  if (Math.abs(percent) >= 100) return '-';
+  return percent;
+};
