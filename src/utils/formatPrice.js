e7e9b7fb3d1e1aa72335b923025bbc21c14123d0
Tam Pham <tampham47@live.com>
2019-01-10 18:20:14 +0700

    enhance price format by actual value (#7)

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -3,5 +3,7 @@ import numeral from 'numeral';
 export default value => {
   if (!value) return 0;
   const n = numeral(value);
+  if (value >= 1000) return n.format('0,0.00');
+  if (value >= 100) return n.format('0,0.0000');
   return n.format('0,0.00000000');
 };
